0dbcbd42037d8a4d16097bea9af8629366a5ad36
BrianSammit <briansammit.cruz@gmail.com>
2021-10-13 21:54:55 -0500

    Style the form

--- src/components/Skate.js
+++ src/components/Skate.js
@@ -57,67 +57,57 @@ const Skate = (props) => {
     const { brand, image } = skate;
 
     return (
-      <div className="skate-item">
-        <h1>{brand}</h1>
-        <img className="ca-img" src={image} alt={brand} />
-        <button className="btn-div">
-          <Link className="btn" to="/skates">
-            Go back
-          </Link>
-        </button>
-      </div>
-    );
-  };
-
-  const generateUserForm = () => {
-    return (
-      <div>
-        <form className="form" onSubmit={handleSubmit}>
-          <div>
-            <label>City</label>
-            <input
-              type="text"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-              placeholder="City"
-              required
-            />
-          </div>
-          <div>
-            <label>Start Date</label>
-            <input
-              type="datetime-local"
-              value={start_date}
-              onChange={(e) => setStartDate(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label>End Date</label>
-            <input
-              type="datetime-local"
-              value={end_date}
-              onChange={(e) => setEndDate(e.target.value)}
-              required
-            />
-          </div>
-          <button className="" type="submit">
-            <div className="text">Submit</div>
+      <div className="skate_form">
+        <div className="skate-item">
+          <h1>{brand}</h1>
+          <img className="ca-img" src={image} alt={brand} />
+          <button className="btn-div">
+            <Link className="btn" to="/skates">
+              Go back
+            </Link>
           </button>
-        </form>
-        <p>{user_id}</p>
-        <p>{start_date}</p>
-        <p>{end_date}</p>
+        </div>
+        <div className="div-test-form">
+          <h2>Wanna try it out</h2>
+          <form className="test_form" onSubmit={handleSubmit}>
+            <div className="label-input">
+              <label>City</label>
+              <input
+                type="text"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                placeholder="City"
+                required
+              />
+            </div>
+            <div className="label-input">
+              <label>Start Date</label>
+              <input
+                type="datetime-local"
+                value={start_date}
+                onChange={(e) => setStartDate(e.target.value)}
+                required
+              />
+            </div>
+            <div className="label-input">
+              <label>End Date</label>
+              <input
+                type="datetime-local"
+                value={end_date}
+                onChange={(e) => setEndDate(e.target.value)}
+                required
+              />
+            </div>
+            <button className="btn-div" type="submit">
+              <div className="btn">Submit</div>
+            </button>
+          </form>
+        </div>
       </div>
     );
   };
 
-  return (
-    <>
-      {generateSkateJSX()}
-      {generateUserForm()}
-    </>
-  );
+  return <>{generateSkateJSX()}</>;
 };
 
 export default Skate;
